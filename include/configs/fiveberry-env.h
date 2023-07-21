096c64c2a1e108ce07ad8920f47fd689849ec8f1
Dmitry Bazhenov <db@aries-embedded.de>
2023-07-22 01:02:51 +0400

    RM6690: refined SPI flash configuration for msrzg2ul
    - add missing define to ultimately enable redundant envornment
    - enable dual-data rate for SPI flash read/write
    - removed unused properties from the SPI node

---
+++ include/configs/fiveberry-env.h
@@ -0,0 +1,57 @@
+#ifndef __FIVEBERRY_ENV_H
+#define __FIVEBERRY_ENV_H
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"bl2_file=boot/bl2_bp-msrzg2ul.bin\0" \
+	"bootargs=rw rootwait earlycon\0" \
+	"bootm_size=0x10000000\0" \
+	"divup_filesize=setexpr filesize ${filesize} / 200 && " \
+		"setexpr filesize ${filesize} + 1\0" \
+	"emmc_bootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p1\0" \
+	"emmc_boot=ext4load mmc 0:1 ${kernel_addr_r} ${image_file} && " \
+		"setenv kernel_comp_size ${filesize} && " \
+		"ext4load mmc 0:1 ${fdt_addr_r} ${fdt_file} && " \
+		"run emmc_bootargs && booti ${kernel_addr_r} - ${fdt_addr_r}\0" \
+	"emmc_update=mmc bootbus 0 2 0 0;mmc partconf 0 0 1 1;" \
+		"tftpboot ${serverip}:${bl2_file};" \
+		"run divup_filesize;mmc write ${fileaddr} 1 ${filesize};" \
+		"tftpboot ${serverip}:${fipfile};" \
+		"run divup_filesize;mmc write ${fileaddr} 0x100 ${filesize}\0" \
+	"emmc_update=run emmc_bl2_update emmc_fip_update\0" \
+	"eth1addr=32:eb:f5:29:04:30\0" \
+	"ethaddr=d6:8f:16:4f:c3:c7\0" \
+	"fdt_addr_r=0x60000000\0" \
+	"fdt_file=boot/msrzg2ul.dtb\0" \
+	"fip_file=boot/fip-msrzg2ul.bin\0" \
+	"image_file=boot/Image.gz\0" \
+	"ipaddr=192.168.1.2\0" \
+	"kernel_addr_r=0x48000000\0" \
+	"kernel_comp_addr_r=0x4c000000\0" \
+	"net_args=setenv bootargs earlycon\0" \
+	"net_boot=tftpboot ${kernel_addr_r} ${serverip}:${image_file} && " \
+		"setenv kernel_comp_size ${filesize} && " \
+		"tftpboot ${ramdisk_addr_r} ${serverip}:${ramdisk_file} && " \
+		"setenv ramdisk_size ${filesize} && " \
+		"tftpboot ${fdt_addr_r} ${serverip}:${fdt_file} && " \
+		"run net_args && " \
+		"booti ${kernel_addr_r} ${ramdisk_addr_r}:${ramdisk_size} ${fdt_addr_r}\0" \
+	"ramdisk_addr_r=0x61000000\0" \
+	"ramdisk_file=boot/fiveberry-image-base-msrzg2ul.cpio.gz\0" \
+	"sd_bootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p1\0" \
+	"sd_boot=ext4load mmc 0:1 ${kernel_addr_r} ${image_file} && " \
+		"setenv kernel_comp_size ${filesize} && " \
+		"ext4load mmc 0:1 ${fdt_addr_r} ${fdt_file} && " \
+		"run sd_bootargs && booti ${kernel_addr_r} - ${fdt_addr_r}\0" \
+	"serverip=192.168.1.1\0" \
+	"spi_bootargs=setenv bootargs earlycon\0" \
+	"spi_boot=sf_probe;sf read ${kernel_addr_r} 0x140000 ${kernel_comp_size};" \
+		"sf read ${ramdisk_addr_r} 0x740000 ${ramdisk_size};" \
+		"sf read ${fdt_addr_r} 0x120000 ${fdt_size};run spi_bootargs;" \
+		"booti ${kernel_addr_r} ${ramdisk_addr_r}:${ramdisk_size} ${fdt_addr_r}\0" \
+	"spi_update=sf probe;sf protect unlock 0 0x100000;sf erase 0 +0x100000;" \
+		"tftpboot ${serverip}:${bl2_file} && sf write ${fileaddr} 0 ${filesize};" \
+		"tftpboot ${serverip}:${fip_file} && sf write ${fileaddr} 0x1d200 ${filesize}\0"
+
+#define CONFIG_BOOTCOMMAND	"run sd_boot || run spi_boot"
+
+#endif /* __FIVEBERRY_ENV_H */
--- include/configs/msrzg2ul.h
+++ include/configs/msrzg2ul.h
@@ -58,28 +58,8 @@
 /* The HF/QSPI layout permits up to 1 MiB large bootloader blob */
 #define CONFIG_BOARD_SIZE_LIMIT		1048576
 
-/* ENV setting */
-
-#define CONFIG_EXTRA_ENV_SETTINGS \
-	"usb_pgood_delay=2000\0" \
-	"bootm_size=0x10000000 \0" \
-	"prodsdbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p1 \0" \
-	"prodemmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p1 \0" \
-	"bootimage=unzip 0x4A080000 0x48080000; booti 0x48080000 - 0x48000000 \0" \
-	"emmcload=ext4load mmc 0:1 0x4A080000 boot/Image.gz;ext4load mmc 0:1 0x48000000 boot/r9a07g043u11-smarc.dts.dtb;run prodemmcbootargs \0" \
-	"sd1load=ext4load mmc 1:1 0x4A080000 boot/Image.gz;ext4load mmc 1:1 0x48000000 boot/r9a07g043u11-smarc.dts.dtb;run prodsdbootargs \0" \
-	"bootcmd_check=if mmc dev 1; then run sd1load; else run emmcload; fi \0" \
-	"ethaddr=d6:8f:16:4f:c3:c7\0" \
-	"eth1addr=32:eb:f5:29:04:30\0" \
-	"ipaddr=192.168.1.2\0" \
-	"ethact=eth0\0" \
-	"serverip=192.168.1.1\0" \
-	"blfile=bl2_bp-msrzg2ul.bin\0" \
-	"fipfile=fip-msrzg2ul.bin\0" \
-	"update=sf probe && sf protect unlock 0 0x1e0000; sf erase 0 +0x1e0000 && " \
-	"tftpboot ${serverip}:${fipfile} && sf write ${loadaddr} 0x11e00 ${filesize}\0"
-
-#define CONFIG_BOOTCOMMAND	"env default -a;run bootcmd_check;run bootimage"
+/* ENV settings */
+#include "fiveberry-env.h"
 
 /* For board */
 /* Ethernet RAVB */
