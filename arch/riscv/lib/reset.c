e086f279b6b273800ab62502b2557dffd7db816b
Dmitry <db@aries-embedded.de>
2023-08-02 00:50:47 +0400

    arch: riscv: implemented do_reset

--- arch/riscv/lib/reset.c
+++ arch/riscv/lib/reset.c
@@ -5,14 +5,25 @@
 
 #include <common.h>
 #include <command.h>
-#include <hang.h>
+#include <cpu_func.h>
+#include <irq_func.h>
+#include <linux/delay.h>
+
+__weak void reset_misc(void)
+{
+}
 
 int do_reset(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
 	printf("resetting ...\n");
 
-	printf("reset not supported yet\n");
-	hang();
+	mdelay(50);				/* wait 50 ms */
 
+	disable_interrupts();
+
+	reset_misc();
+	reset_cpu();
+
+	/*NOTREACHED*/
 	return 0;
 }
