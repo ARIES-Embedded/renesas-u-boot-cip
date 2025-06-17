7d14dc2664c5880f242647ee371d9e2b95cbc1a2
Wolfgang Grandegger <wg@aries-embedded.de>
2025-06-17 12:32:41 +0200

    MSRZG3S: use variable mmc_dev and mmc_part for MMC boot command
    Signed-off-by: Wolfgang Grandegger <wg@aries-embedded.de>

--- include/configs/msrzg3s.h
+++ include/configs/msrzg3s.h
@@ -66,23 +66,25 @@
 /* ENV setting */
 
 #ifdef CONFIG_SUPPORT_EMMC_BOOT
-#define MSRZ_SD_DEV "1:1"
+#define MSRZ_MMC_DEV "1"
 #define MSRZ_FDT_FILE "msrzg3s-baa.dtb"
 #else
-#define MSRZ_SD_DEV "0:1"
+#define MSRZ_MMC_DEV "0"
 #define MSRZ_FDT_FILE "msrzg3s-a0a.dtb"
 #endif
 
-#define CONFIG_EXTRA_ENV_SETTINGS		\
+#define CONFIG_EXTRA_ENV_SETTINGS \
 	"usb_pgood_delay=2000\0" \
 	"bootm_size=0x10000000\0" \
-	"sdbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p1\0" \
+	"mmc_dev=" MSRZ_MMC_DEV "\0" \
+	"mmc_part=1"\0" \
+	"mmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk${mmc_dev}p${mmc_part}\0" \
 	"bootimage=booti 0x48080000 - 0x48000000\0" \
 	"image_file=boot/Image\0" \
 	"fdt_file=boot/" MSRZ_FDT_FILE "\0" \
-	"sdload=ext4load mmc " MSRZ_SD_DEV " 0x48080000 ${image_file};ext4load mmc " MSRZ_SD_DEV " 0x48000000 ${fdt_file};run sdbootargs\0" \
+	"mmcload=ext4load mmc ${mmc_dev}:${mmc_part} 0x48080000 ${image_file};ext4load mmc ${mmc_dev}:${mmc_part} 0x48000000 ${fdt_file}\0" \
 
-#define CONFIG_BOOTCOMMAND	"run sdload bootimage"
+#define CONFIG_BOOTCOMMAND	"run mmcload mmcbootargs bootimage"
 
 /* For board */
 /* Ethernet RAVB */
